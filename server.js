e1ac1a76db65f46da82163bca034a1e8c40b918e
Miqueas Gimenez <miquigz02@gmail.com>
2022-10-23 14:26:04 -0300

    update project, mvc

--- server.js
+++ server.js
@@ -1,17 +1,38 @@
-const express = require('express'); //Referencia a funcion.
-require('dotenv').config();
+// const app = require('express')()
+const express = require('express')
+const { engine } = require('express-handlebars')
+require('dotenv').config()
 
-const app = express();//
-//const app = require('express')();
+const { dbConnection } = require('./database/config')
+const routerIndex = require('./routes')
+const { routerDev } = require('./routes/db')
+const { routerPosts } = require('./routes/posts')
 
-app.get('/', (req, res)=>{
-    res.send('Todo ok');
-})
+// Inicializo la aplicación de express
+const app = express()
 
-const PORT = process.env.PORT;
+// Conectar a la DB
+dbConnection()
 
-app.listen(PORT, err =>{
-    if (err) 
-    throw new Error('Problema con el servidor', err)
-    console.log(`Servidor express escuchando en el puerto: ${PORT}`);
+// Template Engine
+app.engine('hbs', engine({extname: '.hbs'}))
+app.set('view engine', 'hbs')
+app.set('views', './views')
+
+// Middlewares
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+
+
+// Routes
+app.use('/', routerIndex)
+app.use('/', routerDev) // Solo para desarrollo
+app.use('/', routerPosts)
+
+const PORT = process.env.PORT
+app.listen(PORT, err => {
+    if ( err ) throw new Error('Ocurrió un problema con el servidor: ', err)
+    console.log(`Servidor express escuchando en el puerto ${PORT}`)
 })
