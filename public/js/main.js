94bd555e7c375ac02973a5f18e3396e7b4982099
Miqueas Gimenez <miquigz02@gmail.com>
2022-10-27 12:07:29 -0300

    Agrego nav, cambios en los estilos, sweet alert test

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,34 @@
 console.log(document.querySelector('title').textContent)
+
+const swalWithBootstrapButtons = Swal.mixin({
+    customClass: {
+        confirmButton: 'btn btn-success mx-3',
+        cancelButton: 'btn btn-danger mx-3'
+    },
+    buttonsStyling: false,
+    background: "#444444",
+    color: "#FEFEFE"
+})
+
+
+document.querySelector('#botonDelete').addEventListener('click', () => {
+    swalWithBootstrapButtons.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Yes, delete it!',
+        cancelButtonText: 'No, cancel!',
+        reverseButtons: true
+    }).then((result) => {
+        if (result.isConfirmed) {
+            swalWithBootstrapButtons.fire(
+                'Deleted!',
+                'Your file has been deleted.',
+                'success'
+            )
+        }
+    })
+
+})
+
